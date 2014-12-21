bf95201901bcc54228edb046e3486d350f8a51a8
ictxiangxin <ictxiangxin@gmail.com>
2014-12-21 13:44:39 +0800

    add community lib and add player_profile() function

--- community.py
+++ community.py
@@ -5,21 +5,36 @@ from xml.etree import ElementTree
 
 
 def player_profile(steam_id):
-    url = ""
     if isinstance(steam_id, int):
         url = "http://steamcommunity.com/profiles/%d/?xml=1" % steam_id
     elif isinstance(steam_id, str):
         url = "http://steamcommunity.com/profiles/%s/?xml=1" % steam_id
+    else:
+        url = ""
     http = httplib2.Http()
     response, content = http.request(url, "GET")
     if response["status"][0] != "2":
         return None
     profile_et = ElementTree.fromstring(content)
-    tmp_read(profile_et)
-
-
-def tmp_read(et):
-    if isinstance(et, ElementTree.Element):
-        for e in et:
-            tmp_read(e)
-            print(e)
+    profile = {}
+    for entry in profile_et.getchildren():
+        if len(entry) > 0:
+            sub_dict = {}
+            for sub_entry in entry.getchildren():
+                if len(sub_entry) > 0:
+                    ssub_dict = {}
+                    for ssub_entry in sub_entry.getchildren():
+                        ssub_dict[ssub_entry.tag] = ssub_entry.text
+                    if sub_entry.tag == "mostPlayedGame":
+                        ssub_dict["gameID"] = ssub_dict["gameLink"][ssub_dict["gameLink"].rindex("/") + 1:]
+                        sub_dict[ssub_dict["gameName"]] = ssub_dict
+                    elif sub_entry.tag == "group":
+                        sub_dict[ssub_dict["groupID64"]] = ssub_dict
+                    else:
+                        sub_dict[sub_entry.tag] = ssub_dict
+                else:
+                    sub_dict[sub_entry.tag] = sub_entry.text
+            profile[entry.tag] = sub_dict
+        else:
+            profile[entry.tag] = entry.text
+    return profile
